80118c829c25bbebce64588acc56a9246af4ca85
Bruno Sa <brunosantana.engenharia@gmail.com>
2023-02-13 10:06:00 -0300

    Implementado a entidade Unidade

---
+++ src/main/java/com/salonhair/salonhair/entities/Unidade.java
@@ -0,0 +1,102 @@
+package com.salonhair.salonhair.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Unidade implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String rua;
+	private Integer numero;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	
+	public Unidade() {
+		
+	}
+
+	public Unidade(Long id, String rua, Integer numero, String bairro, String cidade, String estado) {
+		super();
+		this.id = id;
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Unidade other = (Unidade) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+
+}

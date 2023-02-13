e501bc3ed111ee8e0c1e2d79300733e9b83585b1
Janaina <tjanaina92@gmail.com>
2023-02-13 11:01:21 -0300

    Implementando a entidade Cliente

---
+++ src/main/java/com/salonhair/salonhair/entities/Cliente.java
@@ -0,0 +1,110 @@
+package com.salonhair.salonhair.entities;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TB_CLIENTE")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	
+	private String nome;
+	
+	@NotNull
+	@Email
+	private String email;
+	
+	@NotNull
+	private String senha;
+	
+	@CPF
+	private int cpf;
+	
+	
+	private int telefone;
+	
+	//Construtores
+	public Cliente() {
+	}
+	
+	public Cliente(Long id, String nome, String email, String senha, int cpf, int telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.cpf = cpf;
+		this.telefone = telefone;
+	}
+
+	//Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", telefone=" + telefone + "]";
+	}
+	
+}

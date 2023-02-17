edb1ba3a12c43e2e4854cc7bb8f46c0105189b3e
Janaina <tjanaina92@gmail.com>
2023-02-17 12:53:11 -0300

    Adição do TestConfig de ClienteModel

--- src/main/java/com/salonhair/salonhair/config/TestConfig.java
+++ src/main/java/com/salonhair/salonhair/config/TestConfig.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.salonhair.salonhair.entities.ClienteModel;
 import com.salonhair.salonhair.entities.UnidadeModel;
+import com.salonhair.salonhair.repositories.ClienteRepository;
 import com.salonhair.salonhair.repositories.UnidadeRepository;
 
 @Configuration
@@ -17,6 +19,9 @@ public class TestConfig implements CommandLineRunner  {
 	@Autowired 
 	private UnidadeRepository unidadeRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		UnidadeModel u1 = new UnidadeModel(null,"Guape",23,"Serodio","Guarulhos","Sp");
@@ -24,6 +29,13 @@ public class TestConfig implements CommandLineRunner  {
 		
 		unidadeRepository.saveAll(Arrays.asList(u1));
 		unidadeRepository.saveAll(Arrays.asList(u2));
+		
+		//testes referente a entidade cliente
+		ClienteModel c1 = new ClienteModel(null, "Jana Java", "janajava@email.com", "senha123", 445335000, 91111111);
+		ClienteModel c2 = new ClienteModel(null, "Ana Nodejs", "ananodejs@email.com", "senha123", 445335001, 91111112);
+		
+		clienteRepository.saveAll(Arrays.asList(c1));
+		clienteRepository.saveAll(Arrays.asList(c2));
 	}
 
 }

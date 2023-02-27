88b76c8417dfa55075c5770b2ef938af8bc5cc1b
Bruno Sa <brunosantana.engenharia@gmail.com>
2023-02-27 09:27:14 -0300

    Implementado a camada repository do profissional

---
+++ src/main/java/com/salonhair/salonhair/repositories/ProfissionalRepository.java
@@ -0,0 +1,9 @@
+package com.salonhair.salonhair.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salonhair.salonhair.entities.ProfissionalModel;
+
+
+public interface ProfissionalRepository  extends JpaRepository<ProfissionalModel, Long>{
+
+}

c6144eddaa7ec1cb7f78e44a050f051a107fbbe0
Janaina <tjanaina92@gmail.com>
2023-02-27 16:15:29 -0300

    Remoção de imports sem uso

--- src/main/java/com/salonhair/salonhair/resources/ProfissionalResource.java
+++ src/main/java/com/salonhair/salonhair/resources/ProfissionalResource.java
@@ -14,11 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.salonhair.salonhair.entities.ProfissionalModel;
-import com.salonhair.salonhair.entities.UnidadeModel;
 import com.salonhair.salonhair.services.ProfissionalService;
-import com.salonhair.salonhair.services.UnidadeService;
 
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value ="/profissional")
@@ -43,7 +40,7 @@ public class ProfissionalResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<ProfissionalModel> insert(@RequestBody @Valid ProfissionalModel obj){
+	public ResponseEntity<ProfissionalModel> insert(@RequestBody ProfissionalModel obj){
 		obj = profissionalService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
